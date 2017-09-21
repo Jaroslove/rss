bd4d7ce41b3ae794f58c84f53c5953f5fa9df860
Jaroslove <jroslovemonro@gmail.com>
2017-09-21 21:24:13 +0300

    first

---
+++ rss/apps/rss_news/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from django.contrib import admin
+from rss.apps.rss_news import views
+
+urlpatterns = [
+    url(r'^index$', views.index),
+    url(r'^$', views.indexAnother),
+]
---
+++ rss/apps/rss_news/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+
+# Create your views here.
+def index(request):
+    number = random.randrange(0, 100)
+    context = {
+        'value': 'Hello Python',
+        'random': str(number)
+    }
+    return render(request, 'index.html', context)
+
+
+def indexAnother(request):
+    number = random.randrange(0, 100)
+    context = {
+        'value': 'Hello Python',
+        'random': str(number)
+    }
+    return render(request, 'MyBootstrap.html', context)
